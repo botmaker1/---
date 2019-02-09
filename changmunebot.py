a4f41d4c815dc3c599e8829226552f1470b68b6d
botmaker1 <47451249+botmaker1@users.noreply.github.com>
2019-02-09 22:02:37 +0900

    Update changmunebot.py

--- changmunebot.py
+++ changmunebot.py
@@ -15,8 +15,8 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith("안녕"):
         await client.send_message(message.channel, "안녕하세요! 창문이-봇입니다! '.도움'을 쳐서 명령어를 알아보세요!")
-    if message.content.startswith(".도움")
-        await client.send_message(message.channel, "창문이-봇의 도움말입니다! 모든 명령어의 맨 앞에는 온점을 붙여주셔야 합니다!")
+        if message.content.startswith(".도움"):
+             await client.send_message(message.channel, "창문이-봇의 도움말입니다. 모든 명령어 앞에는 온점을 붙여주셔야 합니다!")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
