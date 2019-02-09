986765a5045d0bd49b405c4f49dd0d8ff4df2c47
botmaker1 <47451249+botmaker1@users.noreply.github.com>
2019-02-09 23:27:21 +0900

    Update changmunebot.py

--- changmunebot.py
+++ changmunebot.py
@@ -14,10 +14,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content.startswith('.도움'):
-        await client.send_message(message.channel, "창문이-봇의 도움말입니다! 아직 만들어놓은 게 아직 없어요 ㅠㅠ")  
+        await client.send_message(message.channel, "창문이-봇의 도움말입니다! 모든 명령어에는 온점(.)을 붙여주세요! 1. 4딸라  2. 글카")  
     if message.content.startswith('안녕'):
         await client.send_message(message.channel, "안 녕 하 세 요! 창문이-봇입니다! '.도움'을 쳐서 명령어를 알아보세요!")
-        
+    if message.content.startswith('.4딸라'):
+        await client.send_message(message.channel, "오케이, 땡큐, 오케이, __**``4딸라!``**__ (https://m.youtube.com/watch?v=YPcSzJaaKho&t=214s)-이건 4딸라 영상입니다.")
+    if message.content.startswith('.글카'):
+        await client.send_message(message.channel, "가성비를 원하시면 엔비디아 대신 에이엠디로 가시는 걸 추천합니다.")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
