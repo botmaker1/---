b833cca48e6216e2d5c3df281a4beeb570f53950
botmaker1 <47451249+botmaker1@users.noreply.github.com>
2019-02-09 22:44:30 +0900

    Update changmunebot.py

--- changmunebot.py
+++ changmunebot.py
@@ -15,17 +15,15 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith("안녕"):
         await client.send_message(message.channel, "안녕하세요! 창문이-봇입니다! '.도움'을 쳐서 명령어를 알아보세요!")
-    
-    if message.content.startswith(".도움"):
-        await client.send_message(message.channel, "창문이-봇의 도움말입니다. 모든 명령어 앞에는 온점을 붙여주셔야 합니다! .주사위(이건 .주사위를 타이핑 한 후 띄어쓰기 한 후 아무 숫자나 쓴 뒤 .을 붙여주시고 그 뒤에 또 숫자를 붙여주셔야 합니다. ex).주사위 3.5")               
-    
-    if message.content.startswith(".주사위"):
-        roll = message.content.split(" ")
-        rolld = roll[1].split(".")
-        dice = 0
-        for i in range(1, int(rolld[0])+1):
-            dice = dice + random.randint(1, int(rolld[1]))
-        await client.send_message(message.channel, str(dice))
+        if message.content.startswith(".도움"):
+            await client.send_message(message.channel, "창문이-봇의 도움말입니다. 모든 명령어 앞에는 온점을 붙여주셔야 합니다! .주사위(이건 .주사위를 타이핑 한 후 띄어쓰기 한 후 아무 숫자나 쓴 뒤 .을 붙여주시고 그 뒤에 또 숫자를 붙여주셔야 합니다. ex).주사위 3.5")               
+            if message.content.startswith(".주사위"):
+                roll = message.content.split(" ")
+                rolld = roll[1].split(".")
+                dice = 0
+                for i in range(1, int(rolld[0])+1):
+                    dice = dice + random.randint(1, int(rolld[1]))
+                await client.send_message(message.channel, str(dice))
                     
 
 access_token = os.environ["BOT_TOKEN"]
